064e64ef006a9e7825fff7294fe872dce55aa4ee
Jean-Christophe Malapert <Jean-christophe.malapert@cnes.fr>
2023-02-18 06:17:27 +0100

    Fix small bugs

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import setuptools
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, "README.rst"), encoding="utf-8") as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     readme = f.read()
 
 with open("requirements.txt") as f:
