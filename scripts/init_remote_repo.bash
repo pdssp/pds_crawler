36dc38e8ad6b8d85190ab8dc4f91d27c8c104bd1
Jean-Christophe Malapert <Jean-christophe.malapert@cnes.fr>
2023-02-28 02:11:36 +0100

    Init branch

--- scripts/init_remote_repo.bash
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-git branch -M main
-git remote add origin https://github.com/pdssp/pds_crawler.git
-git push -u origin main
--- tests/todo/j_test.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# from pds_crawler.extractor import PdsParserFactory
-# content = ""
-# with open("tests/person3_cat") as f:
-#     content = f.read()
-# result = PdsParserFactory.parse(
-#     content, PdsParserFactory.FileGrammary.PERSON_CAT
-# )
-# print(result)
