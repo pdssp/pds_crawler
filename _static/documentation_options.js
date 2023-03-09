94c08bd5f75d4964835fff3465f9c26219abb6f8
J-Christophe <J-Christophe@users.noreply.github.com>
2023-03-09 03:13:44 +0000

    deploy: f390c029a49b9753f23bc131cfa0c3286f966326

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+gb8d7a51',
+    VERSION: '0.1.dev1+gf390c02',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
